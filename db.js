40b3521dc2d6ce910a2e18380cff493c1681982e
Marco Scheidhuber <github@mail.j23d.de>
2025-10-30 12:46:16 +0000

    Add connection check to db connection.

---
+++ db.js
@@ -0,0 +1,24 @@
+import mariadb from 'mariadb';
+import 'dotenv/config';
+
+const pool = mariadb.createPool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  connectionLimit: 5,
+});
+
+// Verbindung testen
+pool.getConnection()
+  .then(conn => {
+    console.log('Datenbankverbindung erfolgreich');
+    conn.release();
+  })
+  .catch(err => {
+    console.error('Datenbankverbindung fehlgeschlagen:', err);
+  });
+
+
+export default pool;
