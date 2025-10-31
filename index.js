94173838b6f741f07055c7305fc3a04a7562a41c
Marco Scheidhuber <github@mail.j23d.de>
2025-10-31 13:40:36 +0000

    Add profile route and auth middleware.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import pool from './db.js';
 import cors from 'cors';
+import authMiddleware from './middleware/auth.js';
 
 // Prototype overrides
 BigInt.prototype.toJSON = function() { return this.toString() }
@@ -113,6 +114,33 @@ app.post('/api/register', async (req, res) => {
         });
     } finally {
         if (conn) conn.release();
+    }
+});
+
+app.get('/api/profile', authMiddleware, async (req, res) => {
+    const userId = req.user.id;
+
+    const conn = await pool.getConnection();
+
+    try {
+        const [userResult] = await conn.query(
+            `SELECT p.firstname, p.surname, p.bio
+             FROM user u
+             LEFT JOIN user_profile p ON u.id = p.user_id
+             WHERE u.id = ?`,
+            [userId]
+        );
+        if (userResult.length === 0) {
+            return res.status(404).json({ error: 'Profil nicht gefunden' });
+        }
+
+        res.json(userResult);
+
+    } catch (error) {
+        console.error('Fehler beim Abrufen des Profils:', error);
+        res.status(500).json({ error: 'Fehler beim Abrufen des Profils' });
+    } finally {
+        conn.release();
     }
 });
 
